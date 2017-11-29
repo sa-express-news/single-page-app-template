7c7c8195f873e5b4b8f73dc9fd168d85238cbc6e
Kia Farhang <kfarhang0@gmail.com>
2017-11-29 14:38:55 -0600

    Initial setup: Typescript + HTML with a watch task

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import { sayHello } from './module';
+function showHello(divName: string, name: string) {
+    const elt = document.getElementById(divName);
+    elt.innerText = sayHello(name);
+}
+
+showHello("greeting", "Express-New");
